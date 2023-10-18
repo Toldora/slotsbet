2d5e6e5d79deb0708b75f8d036a5bfb7bf0b96ba
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-10-18 13:12:34 +0300

    added open-close logic

--- sign-up.js
+++ sign-up.js
@@ -1,4 +1,10 @@
+const signUpRef = document.querySelector(".js-sign-up");
+const closeBtnRef = signUpRef.querySelector(".js-close-btn");
 const formRef = document.forms.signUp;
+
+const state = {
+  isOpenedModal: false,
+};
 
 // formRef.email.value;
 // formRef.agreeCheck.checked;
@@ -12,3 +18,52 @@ const onSubmit = (event) => {
 formRef.email.addEventListener("input", onInput);
 formRef.agreeCheck.addEventListener("change", onChangeCheckbox);
 formRef.addEventListener("submit", onSubmit);
+
+const preventLinks = (clickEvent, targetElement) => {
+  clickEvent.preventDefault();
+  if (!targetElement || targetElement.tagName === "A") return;
+
+  preventLinks(clickEvent, targetElement.parentNode);
+};
+
+const openModal = () => {
+  signUpRef.classList.add("sign-up__overlay--animation");
+  signUpRef.classList.remove("sign-up__overlay--hidden");
+
+  document.removeEventListener("click", onClickDocument);
+
+  signUpRef.addEventListener("click", onClickOverlay);
+  closeBtnRef.addEventListener("click", closeModal);
+
+  state.isOpenedModal = true;
+};
+
+const closeModal = (event) => {
+  event.stopPropagation();
+
+  signUpRef.classList.remove("sign-up__overlay--animation");
+  signUpRef.classList.add("sign-up__overlay--hidden");
+
+  signUpRef.removeEventListener("click", onClickOverlay);
+  closeBtnRef.removeEventListener("click", closeModal);
+
+  document.addEventListener("click", onClickDocument);
+
+  state.isOpenedModal = false;
+};
+
+const onClickOverlay = (event) => {
+  if (event.target !== event.currentTarget || !state.isOpenedModal) return;
+
+  closeModal(event);
+};
+
+const onClickDocument = (event) => {
+  preventLinks(event, event.target);
+
+  if (state.isOpenedModal) return;
+
+  openModal();
+};
+
+document.addEventListener("click", onClickDocument);
