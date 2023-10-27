36542adb536a5e2c92272e29523a16bc8eeeef08
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-10-27 13:34:19 +0300

    Slotsbet

--- sign-up.js
+++ sign-up.js
@@ -66,7 +66,7 @@ const onSubmit = async (event) => {
     const { ip } = await getIP();
 
     const body = JSON.stringify({
-      name: "nomad",
+      name: "slotsbet",
       email: formRef.email.value,
       browserId,
       ip,
